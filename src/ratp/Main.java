b447e2db950b2d7c8fb0932d95a54e51424516a5
Yvain RAYNAUD <yvain.raynaud@gmail.com>
2021-05-26 17:36:13 +0200

    fix + move after merging

--- src/ratp/Main.java
+++ src/ratp/Main.java
@@ -1,7 +1,5 @@
 package ratp;
 
-import gui.RatpState;
-import gui.RatpStateWithUI;
 import sim.display.Console;
 
 import java.io.IOException;
@@ -9,7 +7,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        RatpState model = new RatpState(System.currentTimeMillis());
+        RatpNetwork model = new RatpNetwork(System.currentTimeMillis());
         RatpStateWithUI gui = new RatpStateWithUI(model);
         Console console = new Console(gui);
         console.setVisible(true);
