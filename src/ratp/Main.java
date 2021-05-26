f7a32259a6db488b84dbd51b885746d44172e299
Yvain RAYNAUD <yvain.raynaud@gmail.com>
2021-05-26 15:03:40 +0200

    moving main to correct place

--- src/ratp/Main.java
+++ src/ratp/Main.java
@@ -1,8 +1,18 @@
 package ratp;
 
+import gui.RatpState;
+import gui.RatpStateWithUI;
+import sim.display.Console;
+
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        RatpState model = new RatpState(System.currentTimeMillis());
+        RatpStateWithUI gui = new RatpStateWithUI(model);
+        Console console = new Console(gui);
+        console.setVisible(true);
+        console.pressPlay();
     }
 }
