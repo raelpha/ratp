48db5447fbca2a65cd92b0424d02426cac010af4
Yvain RAYNAUD <yvain.raynaud@gmail.com>
2021-05-26 14:54:46 +0200

    updating wrong package

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        fr.ratp.RatpState model = new fr.ratp.RatpState(System.currentTimeMillis());
+        RatpState model = new RatpState(System.currentTimeMillis());
         RatpStateWithUI gui = new RatpStateWithUI(model);
         Console console = new Console(gui);
         console.setVisible(true);
