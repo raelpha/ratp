b5b3c6d66e603172894ec55817e4b85e93015bb3
raelpha <raphj@live.fr>
2021-05-30 02:40:56 +0200

    update schedule input data, update data struct, optimize some code

--- src/ratp/lines/Schedules.java
+++ src/ratp/lines/Schedules.java
@@ -13,11 +13,8 @@ import java.util.stream.Collectors;
 
 public class Schedules {
 
-
-    /** Instance unique pré-initialisée */
     private static Schedules INSTANCE = new Schedules();
 
-    /** Point d'accès pour l'instance unique du singleton */
     public static Schedules getInstance()
     {   
         return INSTANCE;
@@ -51,12 +48,13 @@ public class Schedules {
         for(String l : Constants.listOfLinesNames){
             schedules.put(l, new HashMap<>());
         }
+
         //Initialize each list for each line
         for(String line : Constants.listOfLinesNames){
             List<String> servicesNames = new ArrayList<>();
             //Yeah, fuck the complexity...
             for(Schedule s : schedules_by_line.get(line)){
-                servicesNames.add(s.route_long_name);
+                servicesNames.add(s.service);
             }
             servicesNames = servicesNames.stream()
                             .distinct()
@@ -67,40 +65,40 @@ public class Schedules {
             }
 
             for(Schedule s : schedules_by_line.get(line)){
-                schedules.get(line).get(s.route_long_name).add(s);
+                schedules.get(line).get(s.service).add(s);
             }
         }
-        
     }
 
     public static class Schedule {
 
-        public int route_id;
-        public String route_long_name;
+        int entry_id;
+        int order;
+        //TODO: Replace station by station_legacyname -- please use station for now
+        String station_legacyname;
+        String station;
+        String line;
+        int branch;
+        int split;
 
-        public Schedule(int route_id, String route_long_name, int service_id, long trip_id, int trip_headsign, int trip_short_name, int direction_id, int stop_id, String stop_name, String stop_sequence, String line) {
-            this.route_id = route_id;
-            this.route_long_name = route_long_name;
-            this.service_id = service_id;
-            this.trip_id = trip_id;
-            this.trip_headsign = trip_headsign;
-            this.trip_short_name = trip_short_name;
-            this.direction_id = direction_id;
-            this.stop_id = stop_id;
-            this.stop_name = stop_name;
-            this.stop_sequence = stop_sequence;
+        public Schedule(int entry_id, int order, String station_legacyname, String station, String line, int branch, int split, int direction, String station_origin, String station_destination, String service) {
+            this.entry_id = entry_id;
+            this.order = order;
+            this.station_legacyname = station_legacyname;
+            this.station = station;
             this.line = line;
+            this.branch = branch;
+            this.split = split;
+            this.direction = direction;
+            this.station_origin = station_origin;
+            this.station_destination = station_destination;
+            this.service = service;
         }
 
-        public int service_id;
-        public long trip_id;
-        public int trip_headsign;
-        public int trip_short_name;
-        public int direction_id;
-        public int stop_id;
-        public String stop_name;
-        public String stop_sequence;
-        public String line;
+        int direction;
+        String station_origin;
+        String station_destination;
+        String service;
 
     }
 
@@ -110,17 +108,15 @@ public class Schedules {
         List<Schedule> schedules = new ArrayList<>();
 
         try {
-            //String filename = new File(new File(name).getCanonicalPath()).toURI().getPath();
-
             schedules = Files.readAllLines(Paths.get(name))
                     .stream()
                     .skip(2) // 1 for the header, 1 for buggy line Hotel De Ville 1
                     .map(line -> new Schedule(
                             Integer.parseInt(line.split(";")[0]),
-                            line.split(";")[1],
-                            Integer.parseInt(line.split(";")[2]),
-                            Long.parseLong(line.split(";")[3]),
-                            Integer.parseInt(line.split(";")[4]),
+                            Integer.parseInt(line.split(";")[1]),
+                            line.split(";")[2],
+                            line.split(";")[3],
+                            line.split(";")[4],
                             Integer.parseInt(line.split(";")[5]),
                             Integer.parseInt(line.split(";")[6]),
                             Integer.parseInt(line.split(";")[7]),
