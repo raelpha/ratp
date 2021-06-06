a5e0c277e25564e685cbc4f03bc0d02eb6fe29a8
Yvain RAYNAUD <yvain.raynaud@gmail.com>
2021-06-06 11:03:13 +0200

    remove useless things

--- src/ratp/RatpNetwork.java
+++ src/ratp/RatpNetwork.java
@@ -8,23 +8,10 @@ import java.util.Map;
 
 public class RatpNetwork extends SimState {
 
-    /**linesGeomVectorField contains all the LineString of the network*/
-    //Map<String,GeomVectorField> linesGeomVectorField = new HashMap<>();
-
-    /**We WILL make another Graph here for passenger interconnection*/
-    //public GeomPlanarGraph passengerNetwork = new GeomPlanarGraph();
-
     Map<String, Line> lines = LinesDirectory.getInstance().lines;
 
     public RatpNetwork(long seed){
         super(seed);
-
-        /*for (String s : Constants.listOfLinesNames){
-            linesGeomVectorField.put(s, new GeomVectorField(Constants.FIELD_SIZE, Constants.FIELD_SIZE));
-        }*/
-        //FileImporter.shapeFileImporterByLine("lines/lines", linesGeomVectorField);
-
-
     }
 
 }
