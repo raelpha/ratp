a85eac860e6d498429cf616d66a7fd3adb61cbc6
Yvain RAYNAUD <yvain.raynaud@gmail.com>
2021-06-07 14:55:27 +0200

    adding comment

--- src/station/Gare.java
+++ src/station/Gare.java
@@ -6,16 +6,30 @@ import sim.util.geo.MasonGeometry;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * A gare is made of one or more Station.
+ * This class is also used to display associated MasonGeometry with a portrayal
+ */
 public class Gare extends MasonGeometry {
     public int id;
     public String name;
+    public Map<String, Station> stations = new HashMap<>();
 
-
+    /**
+     * Main constructor, used when instantiating new gare (without associated MasonGeometry)
+     * @param id debug value - can be null
+     * @param name
+     */
     public Gare(int id, String name) {
         this.id = id;
         this.name = name;
     }
 
+    /**
+     * This constructor is used to create a Gare from an existing geometry
+     * It is especially used when you want to create a Gare from many Stations
+     * @param gem
+     */
     public Gare(MasonGeometry gem) {
         this.addAttribute(Constants.IS_MULTIPLE_STATION_STR, gem.getStringAttribute(Constants.IS_MULTIPLE_STATION_STR));
         this.addAttribute(Constants.STATION_NAME_STR, gem.getStringAttribute(Constants.STATION_NAME_STR));
@@ -30,8 +44,6 @@ public class Gare extends MasonGeometry {
     public String toString() {
         return name;
     }
-
-    public Map<String, Station> stations = new HashMap<>();
 
     public Station getStation(String lineId){
         return stations.get(lineId);
